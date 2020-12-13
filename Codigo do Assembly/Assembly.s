58e21bf096f9cfb14ff81f92967a931fa09f39ed
Marco Tullio <tulliomarco93@gmail.com>
2020-12-13 20:42:27 -0300

    Codigo em assembly terminado

--- Codigo do Assembly/Assembly.s
+++ Codigo do Assembly/Assembly.s
@@ -1,31 +1,225 @@
 .data
 
-mensagem: 	.asciiz "Digite um numero\n"
-fim: 	.asciiz "Fim\n"
-numero: 	.word 1
-sucessor: 	.word 1
+
+
+mensagem: 	.asciiz "Digite o primeiro numero: "
+mensagem2: 	.asciiz "Digite o segundo numero: "
+saida: 	    .asciiz "Digite a saida desejada: "
+exibir: 	    .asciiz "\n---------------------------------\n"
+erro: 	    .asciiz "Erro: "
+peso1: 	    .asciiz "peso1: "
+peso2: 	    .asciiz "peso2: "
+mySpace:    .asciiz "\n"
+
+p1: 	.float 0.00000000
+p2: 	.float 0.80000000
+taxa: 	.float 0.05
+zeroASFlotat: .float 0.0
+
+
+# f0 = 0.0
+# f1 = primeiro  peso (p1)
+# f2 = segundo  peso (p2)
+# f3 = taxa
+# f4 = primeiro numero a ser somado
+# f5 = segundo numero a ser somado
+# f6 = saida desejada
+# f7 = peso total
+# f8 = erro
+# f9 = variavel temporaria par calculo de peso
+
 
 .text
+    
 
-main:	li $v0, 4
+main:	
+    # zera o f8 
+        lwc1 $f8,zeroASFlotat       
+    # END
+
+     # atribuido pesos a f1 e f2 e taxa de taxa a f3
+	    lwc1 $f1, p1 
+	    lwc1 $f2, p2 
+	    lwc1 $f3, taxa 
+    # END
+
+ Calculo: 
+    # zera o f0 
+        lwc1 $f0,zeroASFlotat       
+    # END
+        
+    # aqui pega o primeiro numero
+
+        # printa uma mensagem
+        li $v0, 4
 		la $a0, mensagem
 		syscall
+        # END
 
-        li $v0, 5 
+         # Le o que o usuario digitou
+        li $v0,6
         syscall
+        # END   
 
-        sw $v0, numero
+        # atribui a f4 o valor que o usuario digitou
+        add.s $f4,$f18,$f0
+        # END
+       
 
-        addi $t0, $v0, 1
+    # END
 
-        sw $t0, sucessor
+    # aqui pega o segundo numero
 
-        li $v0, 1
-        move $a0, $t0
-        syscall
-
+        # printa uma mensagem
         li $v0, 4
-		la $a0, fim
+		la $a0, mensagem2
 		syscall
+        # END
+
+         # Le o que o usuario digitou
+        li $v0,6
+        syscall
+        # END   
+
+        # atribui a f5 o valor que o usuario digitou
+        add.s $f5,$f18,$f0
+    # END
+       
+
+    # aqui pega o valor correto
+
+        # printa uma mensagem
+        li $v0, 4
+		la $a0, saida
+		syscall
+        # END
+
+         # Le o que o usuario digitou
+            li $v0, 6
+            syscall
+        # END   
+
+        # atribui o que o usuario digitou ao f6
+        add.s $f6,$f18,$f0       
+    # END
+
+    # apensa um espaco no texto
+        li $v0, 4 
+	    la $a0, mySpace 
+	    syscall 
+    # END
+
+     # onde a magica acontece(os calculos melhor dizendo)
+
+
+
+        # mutiplica os pesos pelos numeros digitados
+        mul.s $f1,$f1,$f4
+        mul.s $f2,$f2,$f5
+        # END
+
+        # Calcuo do peso total
+        add.s $f7,$f1,$f2,
+        # END
+
+        # Calcuo do erro
+        sub.s $f8,$f6,$f7
+        # END
+
+        # Calculo de novos pesos
+
+            # calculo de peso1
+                mul.s $f9,$f8,$f3
+                mul.s $f9,$f9,$f4
+                add.s $f1,$f1,$f9
+             # END
+
+             # calculo de peso2
+                mul.s $f9,$f8,$f3
+                mul.s $f9,$f9,$f5
+                add.s $f2,$f2,$f9
+             # END
+        # END
+
+   
+    # zera o f0 e o f18
+        lwc1 $f0,zeroASFlotat       
+    # END
+
+    # Erro
+	    li $v0,4
+	    la $a0, exibir
+	    syscall
+
+        li $v0,4
+	    la $a0, erro
+	    syscall
+
+	    # ptintar o numero
+	    li $v0 , 2
+        add.s $f12, $f0, $f8,
+	    syscall
+    # END
+
+  # apensa um espaco no texto
+        li $v0, 4 
+	    la $a0, mySpace 
+	    syscall 
+    # END
+
+     # zera o f0 e o f18
+        lwc1 $f0,zeroASFlotat       
+    # END
+
+    # peso 1
+        li $v0,4
+	    la $a0, peso1
+	    syscall
+
+	    # ptintar o numero
+	    li $v0 , 2
+        add.s $f12, $f0, $f1,
+	    syscall
+    # END
+
+     # zera o f0 e o f18
+        lwc1 $f0,zeroASFlotat       
+    # END
+
+  # apensa um espaco no texto
+        li $v0, 4 
+	    la $a0, mySpace 
+	    syscall 
+    # END
+
+    # peso 2
+        li $v0,4
+	    la $a0, peso2
+	    syscall
+
+	    # ptintar o numero
+	    li $v0 , 2
+        add.s $f12, $f0, $f2,
+	    syscall
+    # END
+
+     # zera o f0 e o f18
+        lwc1 $f0,zeroASFlotat       
+    # END
+
+     # apensa um espaco no texto
+        li $v0, 4 
+	    la $a0, mySpace 
+	    syscall 
+    # END
+     # apensa um espaco no texto
+        li $v0, 4 
+	    la $a0, mySpace 
+	    syscall 
+    # END
+        
+     jr Calculo
+
+
 
         jr $ra
--- teste.s
+++ teste.s
@@ -11,14 +11,52 @@
  myFloat:  .float 3.14
  myDouble: .double 7.202 # o double sai sem os zeros  do float
  zeroDouble: .double 0.0
+ 
+# Pegando o float que o usuario digitar
+
+floatMessage: .asciiz "Digite o numero de PI: "
+zeroASFlotat: .float 0.0
+
+ # Pegando o inteiro que o usuario digitar
+
+prompt: .asciiz "\nDigite sua idade: "
+suaIdade: .asciiz "\nSua idade e: "
+
 		
 # essa parte serve para fazer uma soma de inteiros:
 number1: .word 5
 number2: .word 10
 
+
+# essa parte serve para fazer uma subtracao de inteiros:
+numberSub1: .word 20
+numberSub2: .word 8
+
+
+
 .text
 # qualquer outra inrtucao
  main:
+ 
+	lwc1 $f4,zeroASFlotat # faco f4 = 0.0
+	li $v0,4
+	la $a0, floatMessage
+	syscall
+
+	# ler o que usuario digitar
+
+	li $v0, 6 # 6 e para float
+	syscall
+
+	# mostrar o valor para o usuario
+
+	li $v0 , 2
+	add.s $f12, $f0,$f4 # fazemos essa soma para que o resultado fique em f12 para ser somado
+	syscall
+
+	li $v0, 4 
+	la $a0, mySpace 
+	syscall 
 
 	# printa um hello world
 
@@ -86,6 +124,47 @@ number2: .word 10
 	add $a0,$zero, $t2
 	syscall	
 
+	li $v0, 4 
+	la $a0, mySpace 
+	syscall 
+
+	# somsyba de inteiros
+	lw $s0, numberSub1 # s0 = 20
+	lw $s1, numberSub2 # s0 = 8
+
+	sub $t0,$s0,$s1 # t0 - 20 -8
+
+	li $v0, 1 
+	move $a0,$t0 # isso move o valor de t0 para a0 (ou seja $a0 = $t0)
+	syscall
+
+	# essa parte serve para pegar inteiros que o usuario digitar
+	
+	# Vamos pedir para o usuario digitar a idade
+
+	li $v0, 4
+	la $a0, prompt
+	syscall
+	
+	# pegar a ididade do usuario 
+
+	li $v0,5 # 5 = pegar um inteiro do usuario e que vai ser armazenado em v0
+	syscall
+	
+   # Armazenar os resultados em t0
+
+	move $t0,$v0 # $t0 = $v0
+
+	# mostrar mensagem
+
+	li $v0, 4
+	la $a0, suaIdade
+	syscall
+
+	# ptintar a mensagem
+	li $v0, 1
+	move $a0,$t0 
+	syscall
 
 jr $ra
 
